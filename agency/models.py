13017443f517829570ecebd3b39f4dc09755128a
Taras Savchyn <savik1992@gmail.com>
2023-12-15 23:39:27 +0200

    create models to agency

--- agency/models.py
+++ agency/models.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 
-class Hall(models.Model):
+User = get_user_model()
+
+
+class EventHall(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
@@ -15,13 +18,20 @@ class Hall(models.Model):
         return f"{self.name} {self.city}"
 
 
-class Organizer(AbstractUser):
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+EVENT_TYPE_CHOICES = (
+    ("corporate", "Corporate"),
+    ("wedding", "Wedding"),
+    ("first_date", "First Date"),
+    ("stag", "Stag"),
+    ("girls_party", "Girls Party"),
+    ("working_meeting", "Working Meeting"),
+    ("birthday", "Birthday"),
+    ("christening_of_a_child", "Christening of a child"),
+)
 
 
 class EventType(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, choices=EVENT_TYPE_CHOICES)
 
     def __str__(self):
         return self.name
@@ -32,14 +42,39 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     phone = models.CharField(max_length=13)
     email = models.EmailField()
+    confirmed = models.BooleanField()
 
 
 class Event(models.Model):
-    number_of_guests = models.IntegerField
-    customer = models.ManyToManyField(Customer)
-    event_type = models.ForeignKey(EventType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=63)
+    number_of_guests = models.IntegerField()
+    customer = models.OneToOneField('Customer', on_delete=models.CASCADE)
+    event_type = models.ForeignKey('EventType', on_delete=models.CASCADE)
     date = models.DateField()
-    cost = models.IntegerField()
+    price = models.IntegerField()
+    contractors = models.ManyToManyField('Contractor', related_name='events', blank=True)
+    styles = models.ManyToManyField('Style', related_name='events', blank=True)
+    participants = models.ManyToManyField('User', related_name='events', blank=True)
 
     def __str__(self):
         return f"{self.customer} {self.event_type.name}"
+
+
+STYLE_CHOICES = (
+    ('white', 'White'),
+    ('black', 'Black'),
+    ('pink', 'Pink'),
+    ('green', 'Green'),
+    ('blue', 'Blue'),
+    ('gray', 'Gray'),
+    ('mafia', 'Mafia'),
+)
+
+
+class Style(models.Model):
+    style = models.CharField(max_length=20, choices=STYLE_CHOICES)
+
+
+class Contractor(models.Model):
+    service = models.CharField(max_length=63)
+    price = models.IntegerField()
--- solo_agency/settings.py
+++ solo_agency/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "agency.apps.AgencyConfig",
+    "agency",
 ]
 
 MIDDLEWARE = [
