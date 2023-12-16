eb5e7e007033b879f65d813a70f127ffa2a78412
Taras Savchyn <savik1992@gmail.com>
2023-12-16 22:12:48 +0200

    add file serialisers.py & urls.py to agency

---
+++ agency/urls.py
@@ -0,0 +1,14 @@
+# from rest_framework import routers
+#
+# from agency.views import (
+#     OrganizerViewSet,
+# )
+#
+# router = routers.DefaultRouter()
+#
+# router.register(r"organizers", OrganizerViewSet)
+#
+#
+# urlpatterns = router.urls
+#
+# app_name = "agency"
