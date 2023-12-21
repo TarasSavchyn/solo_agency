dead9c3fe06e8c8f88a948098de4fca6b765bca7
Taras Savchyn <savik1992@gmail.com>
2023-12-21 23:34:19 +0200

    redid file admin.py

--- agency/admin.py
+++ agency/admin.py
@@ -1,50 +1,35 @@
-# from django.contrib import admin
-# from agency.models import (EventHall, EventType, Event, Contractor, Order, Organizer, HallOption, Guest)
-#
-#
-# @admin.register(EventHall)
-# class EventHallAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'city', 'address', 'maximum_number_of_guests', 'phone', 'email')
-#     search_fields = ('name', 'city')
-#
-#
-# @admin.register(EventType)
-# class EventTypeAdmin(admin.ModelAdmin):
-#     list_display = ('name',)
-#     search_fields = ('name',)
-#
-#
-# @admin.register(Event)
-# class EventAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'customer', 'event_type', 'date', 'price')
-#     search_fields = ('name', 'customer__first_name', 'customer__last_name', 'event_type__name')
-#
-#
-# @admin.register(Contractor)
-# class ContractorAdmin(admin.ModelAdmin):
-#     list_display = ('service', 'price', 'phone', 'email')
-#     search_fields = ('service',)
-#
-#
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'date', 'event')
-#     search_fields = ('user__first_name', 'user__last_name', 'event__name')
-#
-#
-# @admin.register(Organizer)
-# class OrganizerAdmin(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name', 'position', 'phone', 'email')
-#     search_fields = ('first_name', 'last_name', 'position')
-#
-#
-# @admin.register(HallOption)
-# class HallOptionAdmin(admin.ModelAdmin):
-#     list_display = ('name',)
-#     search_fields = ('name',)
-#
-#
-# @admin.register(Guest)
-# class GuestAdmin(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name', 'adult', 'sex', 'phone', 'email')
-#     search_fields = ('first_name', 'last_name')
+from django.contrib import admin
+from .models import Service, Agency, EventType, Organizer, Event, Advice, Review
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'agency_values')
+    filter_horizontal = ('services',)  # Додавання фільтрації для багато-до-багатьох відносин
+
+@admin.register(EventType)
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(Organizer)
+class OrganizerAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'position', 'phone', 'email')
+    search_fields = ('first_name', 'last_name', 'position')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event_type', 'date', 'user')
+    filter_horizontal = ('organizers',)  # Додавання фільтрації для багато-до-багатьох відносин
+
+@admin.register(Advice)
+class AdviceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'rating', 'created_at', 'is_approved')
+    list_filter = ('is_approved',)
+    search_fields = ('user__username', 'text')
