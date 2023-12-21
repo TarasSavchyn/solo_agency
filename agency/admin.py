da8c91392767a8b80985ed78ca52bceef2af5d81
Taras Savchyn <savik1992@gmail.com>
2023-12-21 23:41:20 +0200

    redid file serializers.py

--- agency/admin.py
+++ agency/admin.py
@@ -1,35 +1,42 @@
 from django.contrib import admin
 from .models import Service, Agency, EventType, Organizer, Event, Advice, Review
 
+
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = ("name", "description")
+
 
 @admin.register(Agency)
 class AgencyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'agency_values')
-    filter_horizontal = ('services',)  # Додавання фільтрації для багато-до-багатьох відносин
+    list_display = ("name", "description", "agency_values")
+    filter_horizontal = ("services",)
+
 
 @admin.register(EventType)
 class EventTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = ("name", "description")
+
 
 @admin.register(Organizer)
 class OrganizerAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'position', 'phone', 'email')
-    search_fields = ('first_name', 'last_name', 'position')
+    list_display = ("full_name", "position", "phone", "email")
+    search_fields = ("first_name", "last_name", "position")
+
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('name', 'event_type', 'date', 'user')
-    filter_horizontal = ('organizers',)  # Додавання фільтрації для багато-до-багатьох відносин
+    list_display = ("name", "event_type", "date", "user")
+    filter_horizontal = ("organizers",)
+
 
 @admin.register(Advice)
 class AdviceAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer')
+    list_display = ("question", "answer")
+
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('user', 'text', 'rating', 'created_at', 'is_approved')
-    list_filter = ('is_approved',)
-    search_fields = ('user__username', 'text')
+    list_display = ("user", "text", "rating", "created_at", "is_approved")
+    list_filter = ("is_approved",)
+    search_fields = ("user__username", "text")
--- agency/serializers.py
+++ agency/serializers.py
@@ -1,61 +1,76 @@
-# from rest_framework import serializers
-# from .models import EventHall, EventType, Event, Contractor, Order, Organizer, HallOption, Guest
-#
-#
-# class HallOptionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = HallOption
-#         fields = ['id', 'name', 'description']
-#
-#
-# class EventHallSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = EventHall
-#         fields = ['id', 'name', 'city', 'address', 'maximum_number_of_guests', 'options', 'phone', 'email']
-#
-#
-# class EventTypeSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = EventType
-#         fields = ['id', 'name']
-#
-#
-# class ContractorSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Contractor
-#         fields = ['id', 'service', 'price', 'phone', 'email']
-#
-#
-# class GuestSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Guest
-#         fields = ['id', 'first_name', 'last_name', 'adult', 'sex', 'phone', 'email']
-#
-#
-# class EventSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Event
-#         fields = [
-#             'id',
-#             'name',
-#             'number_of_guests',
-#             'customer',
-#             'event_type',
-#             'date',
-#             'price',
-#             'contractors',
-#             'style',
-#             'guests'
-#         ]
-#
-#
-# class OrderSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Order
-#         fields = ['id', 'user', 'date', 'event']
-#
-#
-# class OrganizerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Organizer
-#         fields = ['id', 'first_name', 'last_name', 'position', 'events_organized', 'phone', 'email']
+from rest_framework import serializers
+from .models import Service, Agency, EventType, Organizer, Event, Advice, Review
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ("id", "name", "description")
+
+
+class AgencySerializer(serializers.ModelSerializer):
+    services = ServiceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Agency
+        fields = ("id", "name", "description", "agency_values", "services")
+
+
+class EventTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventType
+        fields = ("id", "name", "description")
+
+
+class OrganizerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organizer
+        fields = (
+            "id",
+            "description",
+            "first_name",
+            "last_name",
+            "position",
+            "phone",
+            "email",
+            "full_name",
+        )
+
+
+class EventSerializer(serializers.ModelSerializer):
+    organizers = OrganizerSerializer(many=True)
+
+    class Meta:
+        model = Event
+        fields = (
+            "id",
+            "organizers",
+            "description",
+            "name",
+            "number_of_guests",
+            "event_type",
+            "date",
+            "style",
+            "user",
+            "created_at",
+        )
+
+
+class AdviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Advice
+        fields = ("id", "question", "answer")
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = (
+            "id",
+            "user",
+            "text",
+            "rating",
+            "created_at",
+            "updated_at",
+            "is_approved",
+        )
